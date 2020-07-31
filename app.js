12feec6a158a7a68c454cfc9bf0598e3e04b12b5
AbdiM86 <a.mohamed@aliaslab.net>
2020-07-31 23:18:07 +0200

    add front end form changed

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const { json } = require('express');
 app.set("view engine","ejs");
 
 var port = process.env.PORT || 3000;
-const callAPIState = () =>{
+
+app.use(express.static(__dirname + '/public'));
+
+const callAPIState = async ( ) =>{
  //https://api.printful.com/countries
  axios.get('https://api.printful.com/countries').then(data=>{
-    return data;
-
+   
+   return data
  }).catch(error=>{
      console.log(error)
  })
 }
 
-app.get('*',function(req,res){
-   //let data=  callAPIState();
-    res.render("pages/index",{message:"Contattaci"})
+app.get('*',  function(req,res){
+
+  axios.get('https://api.printful.com/countries').then(data=>{
+    
+    res.render("pages/index",{message:"Contattaci", stati: data.data.result})
+
+ }).catch(error=>{
+     console.log(error)
+ })
+    
+   
 
 })
 app.post('/contact',function(req,res){
      console.log("req.body");
-     res.render("pages/index",{message:"Contattaci"})
+    res.json({result:'ok'})
  })
 
 app.listen(port,function(){
